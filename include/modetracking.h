16e6c8c2c7a0d878105ec4f569224711242ddadf
Chris <clupp@umich.edu>
2019-11-01 23:45:25 -0400

    added new file banner

--- include/modetracking.h
+++ include/modetracking.h
@@ -1,20 +1,21 @@
-//==============================================================================
-//
-//     __  ___          __      __   ______            __   __ __ _ __
-//    /  |/  /___  ____/ /___ _/ /  /_  __/___  ____  / /  / //_/(_) /_
-//   / /|_/ / __ \/ __  / __ `/ /    / / / __ \/ __ \/ /  / ,<  / / __/
-//  / /  / / /_/ / /_/ / /_/ / /    / / / /_/ / /_/ / /  / /| |/ / /
-// /_/  /_/\____/\__,_/\__,_/_/    /_/  \____/\____/_/  /_/ |_/_/\__/
-//
-//
-// University of Michigan
-// Active Aeroelasticity and Structures Research Lab
-//
-// Author: Christopher A. Lupp
-//
-// Mode Tracking
-//
-//==============================================================================
+/*
+    Modal Tool Kit (MTK)
+
+    Copyright 2017-2019 Christopher A. Lupp
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
 #pragma once
 
 // system headers
