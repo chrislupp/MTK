4f665c86ed245d005ff488e9a0f8914bdef41ee1
Chris <clupp@umich.edu>
2020-01-28 18:45:39 -0500

    added unit testing for eigenpair

--- test/eigenpair.py
+++ test/eigenpair.py
@@ -1,5 +1,57 @@
 """
-    MTK EigenPair Tests
+    Modal Tool Kit (MTK)
 
-    Author: Christopher A. Lupp
+    Copyright 2017-2020 Christopher A. Lupp
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
 """
+import unittest
+
+from MTK.MTK import EigenPair
+
+
+class CheckEigenPair(unittest.TestCase):
+
+    def test_DefaultEigenPairCreation(self):
+        """
+        Tests the basic instantiation of an EigenPair.
+        """
+        a = EigenPair()
+
+
+    def test_AlternateEigenPairCreation(self):
+        """
+        Tests the basic instantiation of an EigenPair with initial values.
+        """
+        a = EigenPair(1.0, [ 0.0, 1.0])
+        self.assertTrue(a["evalue"] == 1.0)
+        self.assertTrue((a["evector"] == [ 0.0, 1.0 ]).all())
+
+
+    def test_AlternateEigenPairCreationComplex(self):
+        """
+        Tests the basic instantiation of an EigenPair with initial complex
+        values.
+        """
+        a = EigenPair(1.0 + 1.0*1j, [ 0.0, 1.0 + 1.0 * 1j])
+        self.assertTrue(a["evalue"] == 1.0 + 1*1j)
+        self.assertTrue((a["evector"] == [ 0.0, 1.0 + 1.0*1j]).all())
+
+
+
+def main():
+    unittest.main()
+
+
+if __name__ == "__main__":
+    main()
